d77f315d6484836576b969d86bfc21235088f579
Ya <qsnhywadej@gmail.com>
2017-07-14 00:52:56 +0500

    first, but not last

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* Route */
+
+angular.module('openWeatherApp', [
+  'ngRoute',
+  'openWeatherApp.filters',
+  'openWeatherApp.services',
+  'openWeatherApp.directives',
+  'openWeatherApp.controllers',
+  "iso-3166-country-codes"
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/forecast', {templateUrl: 'partials/forecast.html', controller: 'OpenWeatherCtrl'});
+  $routeProvider.when('/storm', {templateUrl: 'partials/storm.html', controller: 'OpenWeatherCtrl'});
+  $routeProvider.otherwise({redirectTo: '/forecast'});
+}]);
---
+++ js/directives.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/* Directives */
+
+angular.module('openWeatherApp.directives', [])
+
+  .directive('appVersion', ['version', function(version) {
+    return function(scope, elm, attrs) {
+      elm.text(version);
+    };
+  }])
+
+  .directive('weatherPanel',[function factory() {
+    return {
+      restrict: 'EA',
+
+      scope: {
+        useDayForecast: '=showEntry',
+        forecast: '=weatherPanel'
+      },
+
+      templateUrl: 'partials/_weather-panel-light.html',
+
+      link: function(scope, element, attrs) {
+        scope.getIconImageUrl = function(iconName) {
+          return (iconName ? 'http://openweathermap.org/img/w/' + iconName + '.png' : '');
+        };
+
+        scope.parseDate = function (time) {
+          return new Date(time * 1000);
+        };
+      }
+    }
+  }])
+
+
+.directive('weatherPanelWind',[function factory() {
+  return {
+    restrict: 'EA',
+
+    scope: {
+      useDayForecast: '=showEntry',
+      forecast: '=weatherPanel'
+    },
+
+    templateUrl: 'partials/_weather-panel-wind.html',
+
+    link: function(scope, element, attrs) {
+      scope.getIconImageUrl = function(iconName) {
+        return (iconName ? 'http://openweathermap.org/img/w/' + iconName + '.png' : '');
+      };
+
+      scope.parseDate = function (time) {
+        return new Date(time * 1000);
+      };
+    }
+  }
+}]);
+
